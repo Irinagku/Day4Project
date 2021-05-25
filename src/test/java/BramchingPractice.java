11df287c4ccc65ae230dd0fd2d6e6c58b67e2098
Irina Tulus <irinagku@gmail.com>
2021-05-25 19:23:49 -0400

    shared on GitHub

--- src/test/java/BramchingPractice.java
+++ src/test/java/BramchingPractice.java
@@ -1,5 +1,9 @@
 public class BramchingPractice {
     public static void main(String[] args) {
+        // created new project
+        // enable version control
+        // added .gitignore with .idea , target folder and  .iml file
+        // made initial commit
 
     }
 }
